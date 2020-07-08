545caca46d5c4e788fa6f8965fc083a30aef51e8
hasuke0324 <65121426+hasuke0324@users.noreply.github.com>
2020-07-08 23:55:46 +0900

    ajax-delete-task

--- app/assets/javascripts/delete.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-  $('.task_delete_btn').on('click', function(){
-    $('<div class = "task">').remove();
-  });
-});
---
+++ app/assets/javascripts/destroy.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('task_delete_btn').on('click', function(e){
+    e.preventDefault();
+    var url = $(this).attr('action')
+
+    $.ajax({
+      url: "/tasks/" + TaskID,
+      type: "POST",
+      data: {"id":TaskID,"_method": "DELETE"}
+    })
+  });
+});
