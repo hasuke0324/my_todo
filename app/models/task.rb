76976438c27244e3e04a8af0ae410c05d3fa3370
hasuke0324 <65121426+hasuke0324@users.noreply.github.com>
2020-08-06 14:33:38 +0900

    Update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 26 }
   attribute :priority, :string, default: '未設定'
 
   belongs_to :user
